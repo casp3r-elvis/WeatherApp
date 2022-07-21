c5f570814befd26229a56ea50328082df24a8703
ghost-cas <esike.akpos@gmail.com>
2022-07-21 11:33:44 +0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "63ec27b8a4msh0b5e373f90df29fp191516jsn337dd63ed11e",// enter your rapid api key here
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "e98d89a9db160fb58fcd527460d2bc39"; // enter your key from openweather API
